2c6dff5069f17693c7b5fe3100eb9834db841651
bryanmojica <bryan@trestian.com>
2016-08-29 20:01:04 -0700

    establish routing for /api/lyrics

--- client/controllers/queryController.js
+++ client/controllers/queryController.js
@@ -2,11 +2,20 @@ var herdlyApp = angular.module('herdlyApp', []);
 
 herdlyApp.controller('QueryController', ['$scope', '$http', function ($scope, $http) {
 
+		// when landing on page, get previous lyrics and dispaly them
+  $http.get('/api/lyrics')
+       .success(function(data) {
+         $scope.lyrics = data;
+         console.log(data);
+       })
+       .error(function(data) {
+         console.log('error: ' + data);
+       });
+
   console.log('controller is hooked up!');
 
   var apiURL = 'http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=';
   var apiKey = '&api_key= 5c05369bfa338d8a5c3ec52d18663241&format=json';
-
 
   // take username on click submission
   // fire GET request
@@ -16,30 +25,17 @@ herdlyApp.controller('QueryController', ['$scope', '$http', function ($scope, $h
     var username = $scope.newUser;
 
     $http.get(apiURL + username + apiKey).then(function (response) {
-      console.log(response.data, 'RESPONSE!')});
+      console.log(response.data, 'RESPONSE!');
+      
+      taggy = response.data.lovedtracks.track;
+
+      for ( var i = 0; i < taggy.length; i++ ) {
+        var trackArtist = taggy[i].artist.name;
+        var trackName = taggy[i].name;
+        console.log('loved track is', trackArtist + ' : ' + trackName);
+      }
+    });
   };
 
-	
-	// last FM querying works: loved tracks
-  $http.get('http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=bryytunes&api_key= 5c05369bfa338d8a5c3ec52d18663241&format=json').then(function (response) {
-    console.log('MUSIC TAG IN THIS PARTICULAR SPOT RIGHT HERE IS....', response.data);
-
-    taggy = response.data.lovedtracks.track;
-    
-    for ( var i = 0; i < taggy.length; i++ ) {
-      var trackArtist = taggy[i].artist.name;
-      var trackName = taggy[i].name;
-      console.log('loved track is', trackArtist + ' : ' + trackName);
-    }
-  });
-
-	// musicNLyrics API
-/*	$http.get('http://api.lyricsnmusic.com/songs?api_key=83be8d44f3786545c46de607f07345&page=2&q=coldpay%20clocks').then(function (response) {
-		console.log('RESPONSE DATA IS', response.data);
-		taggy = response.data.toptags.tag[0].name;
-		console.log('MUSIC TAG IN THIS PARTICULAR SPOT RIGHT HERE IS....', taggy);
-	});*/
-
-	
 
 }]);
--- server.js
+++ server.js
@@ -8,20 +8,12 @@ mongoose.connect('mongodb://localhost/herdly');
 // serve up static files
 app.use(express.static(__dirname + '/client'));
 
-app.get('/', function (req, res) {
-  if ( req.method === 'GET' ) {
-    console.log('we are live and listening -- herd.ly');	
-  }
-});
 
-// 404 service
-app.get('/*', function (req, res) {
-  res.send('are you lost? must be bc this here is our 404 notice. click yr heels and get back to the index.');
-});
+// MONGO / DB SETUP ------------------------------------------------------------------
 
 // init Mongo schema
 
-var LyricSchema = new mongoose.Schema({
+var lyricSchema = new mongoose.Schema({
   artist: {
     type: String,
     required: true
@@ -31,6 +23,39 @@ var LyricSchema = new mongoose.Schema({
     type: String,
     required: true
   }
+});
+
+var Lyric = mongoose.model('Lyric', lyricSchema);
+
+var newLyric = new Lyric({ artist: 'test', lyric: 'wooah-oh' });
+
+// ROUTES ----------------------------------------------------------------------------
+
+app.get('/', function (req, res) {
+  if ( req.method === 'GET' ) {
+    console.log('we are live and listening -- herd.ly');	
+  }
+});
+
+app.post('/api/lyrics', function(req, res) {
+// generate a new lyric, information comes from AJAX request from Angular
+  Lyric.create({
+    artist: req.body,
+    lyric: req.body
+  });
+  console.log('we got a post req');
+});
+
+app.get('/api/lyrics', function(req, res) {
+// generate a new lyric, information comes from AJAX request from Angular
+  res.send('THIS IS OUR CURRENT /api/lyrics RESPONSE');
+
+  console.log('we got a post req');
+});
+
+// 404 service
+app.get('/*', function (req, res) {
+  res.send('are you lost? must be bc this here is our 404 notice. click yr heels and get back to the index.');
 });
 
 app.listen(1337);
