e5cf4030c2577d7b26ea0354d78c0d5c41f61259
bryanmojica <bryan@trestian.com>
2016-08-29 19:09:46 -0700

    pass username from view to controller

---
+++ client/controllers/queryController.js
@@ -0,0 +1,45 @@
+var herdlyApp = angular.module('herdlyApp', []);
+
+herdlyApp.controller('QueryController', ['$scope', '$http', function ($scope, $http) {
+
+	var vm = this;
+
+	console.log('controller is hooked up!');
+
+	var taggy;
+
+	$scope.genLyric = function () {
+   console.log('proving click handler', $scope.newUser);
+	};
+
+	// toying with LASTFM API
+	/*$http.get('http://ws.audioscrobbler.com/2.0/?method=user.gettoptags&user=bryytunes&api_key= 5c05369bfa338d8a5c3ec52d18663241&format=json').then(function (response) {
+		console.log('RESPONSE DATA IS', response.data);
+		taggy = response.data.toptags.tag[0].name;
+		console.log('MUSIC TAG IN THIS PARTICULAR SPOT RIGHT HERE IS....', taggy);
+	});*/
+
+	
+	// last FM querying works: loved tracks
+	$http.get('http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=bryytunes&api_key= 5c05369bfa338d8a5c3ec52d18663241&format=json').then(function (response) {
+		console.log('MUSIC TAG IN THIS PARTICULAR SPOT RIGHT HERE IS....', response.data);
+
+		taggy = response.data.lovedtracks.track;
+		for ( var i = 0; i < taggy.length; i++ ) {
+			var trackArtist = taggy[i].artist.name;
+			var trackName = taggy[i].name;
+			console.log('loved track is', trackArtist + ' : ' + trackName);
+		}
+		
+	});
+
+	// musicNLyrics API
+/*	$http.get('http://api.lyricsnmusic.com/songs?api_key=83be8d44f3786545c46de607f07345&page=2&q=coldpay%20clocks').then(function (response) {
+		console.log('RESPONSE DATA IS', response.data);
+		taggy = response.data.toptags.tag[0].name;
+		console.log('MUSIC TAG IN THIS PARTICULAR SPOT RIGHT HERE IS....', taggy);
+	});*/
+
+	
+
+}]);
--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
 var app = express();
 
+mongoose.connect('mongodb://localhost/herdly');
+
 // serve up static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client'));
 
 app.get('/', function (req, res) {
   if ( req.method === 'GET' ) {
-    console.log('we are live and listening -- herd.ly, copyright 2k16');	
+    console.log('we are live and listening -- herd.ly');	
   }
 });
+
+/*// handle username submissions from client
+app.post('/api/links', funciton (req, res) {
+  console.log(req);
+});*/
 
 // 404 service
 app.get('/*', function (req, res) {
   res.send('are you lost? must be bc this here is our 404 notice. click yr heels and get back to the index.');
+});
+
+// init Mongo schema
+
+var Schema = mongoose.Schema;
+
+var lyricSchema = new Schema({
+  artist:  String,
+  lyric: String
 });
 
 app.listen(1337);
